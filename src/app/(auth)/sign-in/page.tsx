83732035754eae7ea82d7cd65e7d46c857e12b3c
sawsew467 <mac@MacBook-Pro.local>
2024-04-19 22:58:50 +0700

    feat: sign in page ui

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,48 +1,49 @@
-"use client"
+'use client';
 
 import { postRequest } from '@/services/request';
-import { Button } from 'antd'
+import { Button } from 'antd';
 import { setCookie } from 'cookies-next';
 import { useRouter } from 'next/navigation';
-import React from 'react'
+import React from 'react';
 
 function page() {
-    const router = useRouter();
-    const handleSignIn = async () => {
-        try {
-          const data = {
-            username: "ledinhdangkhoa10a9@gmail.com",
-            password: "Admin123@",
-            rememberMe: true,
-          };
-          // await signIn(data);
-          const res: any = await postRequest(
-            "https://fudeverwebapi.io.vn/api/Auth/sign-in",
-            {
-              data,
-            }
-          );
-          console.log(res?.body?.accessToken);
-    
-          setCookie("next_token", res?.body?.accessToken);
-          router.push("/");
-        } catch (error) {
-          console.log(error);
-        }
+  const router = useRouter();
+  const handleSignIn = async () => {
+    try {
+      const data = {
+        username: 'ledinhdangkhoa10a9@gmail.com',
+        password: 'Admin123@',
+        rememberMe: true,
       };
-    return (
-        <div style={
-            {
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-                justifyContent: "center"
-            }
-        }>
-            Sign in
-            <Button onClick={() => handleSignIn()}>Login</Button>
-        </div>
-    )
+      // await signIn(data);
+      const res: any = await postRequest(
+        'https://fudeverwebapi.io.vn/api/Auth/sign-in',
+        {
+          data,
+        },
+      );
+      console.log(res?.body?.accessToken);
+
+      setCookie('next_token', res?.body?.accessToken);
+      router.push('/');
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      Sign in
+      {/* thang da code */}
+      <Button onClick={() => handleSignIn()}>Login</Button>
+    </div>
+  );
 }
 
-export default page
+export default page;
