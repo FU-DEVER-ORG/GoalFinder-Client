3c8c36f7f3b7f03c6e045e6c09afe00c8f3e9731
Saugao274 <thuynttde170035@fpt.edu.vn>
2024-04-21 00:45:05 +0700

    UI step 2 (OTP verification)

---
+++ src/components/modules/ForgotPassword/OTPMail/index.tsx
@@ -0,0 +1,44 @@
+'use client';
+import * as S from './styles';
+import Image from 'next/image';
+import Typography from '@/components/core/common/Typography';
+import { IProps } from '../Main';
+import FormAdd from '../FormAdd';
+function OTPMail(props: IProps) {
+  return (
+    <S.AddMailWrapper>
+      <Typography variant="h3">E-mail Verification</Typography>
+      <S.ImgLogo>
+        <Image
+          alt=""
+          src={'/images/forgot-password/logo.png'}
+          width={150}
+          height={150}
+        />
+      </S.ImgLogo>
+      <S.TypographyWrapper>
+        {props.navigation === 'step1' ? (
+          <>
+            <Typography variant="h5">Add your gmail</Typography>
+            <Typography style="italic">
+              Enter your gmail in order to send you your OTP security code
+            </Typography>
+          </>
+        ) : (
+          <>
+            <Typography variant="h5">Gmail verification</Typography>
+            <Typography style="italic">
+              A OTP code has been sent to ****@gmail.com, please fill the form
+            </Typography>
+          </>
+        )}
+      </S.TypographyWrapper>
+      <FormAdd
+        navigation={props.navigation}
+        setNavigation={props.setNavigation}
+      />
+    </S.AddMailWrapper>
+  );
+}
+
+export default OTPMail;
