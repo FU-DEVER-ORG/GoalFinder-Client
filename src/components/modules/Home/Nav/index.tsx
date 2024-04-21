8506dbf66da7aaa097560a751d2cc9e4ec538b89
Khangnvd <khangnguyen.030204@gmail.com>
2024-04-21 23:15:38 +0700

    Nav UI

--- src/components/modules/Home/Nav/index.tsx
+++ src/components/modules/Home/Nav/index.tsx
@@ -1,13 +1,35 @@
 import BoxFlex from '@/components/core/common/BoxFlex';
 
 import * as S from './styles';
+import { EllipsisOutlined, HeartOutlined, HistoryOutlined, HomeOutlined } from '@ant-design/icons';
+import Sider from 'antd/es/layout/Sider';
+import { Menu, MenuProps, theme } from 'antd';
+import React from 'react';
+import { color } from 'framer-motion';
+
+const options = ["Trang chủ", "Lưu trữ", "Lịch sử đăng ký", "Xem thêm"];
+const icons = [HomeOutlined, HeartOutlined, HistoryOutlined, EllipsisOutlined];
+const items: MenuProps['items'] = icons.map((icon, key) => ({
+  key: `${key}`,
+  icon: React.createElement(icon),
+  label: `${options[key]}`,
+}));
+
 
 function Nav() {
+  console.log(items);
+  
   return (
     <BoxFlex vertical gap={16}>
-      nav
+      <S.Sider >
+        <S.MenuCss
+          mode="inline"
+          items={items}
+        >
+        </S.MenuCss>
+      </S.Sider>
     </BoxFlex>
   );
-}
+};
 
 export default Nav;
--- src/components/modules/Home/Nav/styles.ts
+++ src/components/modules/Home/Nav/styles.ts
@@ -1,1 +1,30 @@
+import { Menu } from 'antd';
 import styled from 'styled-components';
+
+export const BoxFlex = styled.nav``;
+
+export const Sider = styled.nav``;
+
+export const MenuCss = styled(Menu)`
+  width: 280px;
+  padding: 20px;
+  gap: 12px;
+  border-radius: 8px 0px 0px 0px;
+  opacity: 0px;
+  border-inline-end: 0px !important;
+  .ant-menu-item {
+    width: 240px !important;
+    background-color: #ffffff;
+    border-radius: 8px;
+    padding: 12px, 20px, 12px, 20px !important;
+    margin: 0px !important;
+    gap: 12px;
+  }
+  .ant-menu-item-selected {
+    background-color: #f1fff2 !important;
+    color: #4daa57 !important;
+  }
+  .ant-menu-divider {
+    background-color: red !important;
+  }
+`;
