b78bcd7d768f4d15c8d0d3630cfa72fff4cd14b3
Khangnvd <khangnguyen.030204@gmail.com>
2024-04-24 00:06:18 +0700

    fix font-size of Nav-UI and update date by moment

--- src/components/modules/Home/YourMatch/index.tsx
+++ src/components/modules/Home/YourMatch/index.tsx
@@ -1,40 +1,32 @@
 'use client';
 
-import { MenuProps } from 'antd';
-
+import moment from 'moment';
 import * as S from './styles';
-import { MenuItemGroupType, MenuItemType, SubMenuType } from 'antd/es/menu/hooks/useItems';
 
 interface MenuItem {
   label?: string;
   date?: string;
-  time?: string;
   curentNumber?: number;
   maxNumber?: number;
   href?: string;
 }
-type MyItemType = MenuItemType | SubMenuType | MenuItemGroupType;
 
 const listItems: MenuItem[] = [
-  { label: "Kèo anh em văn phòng đấm nhau", date: "", time: "16/04/2024", curentNumber: 0, maxNumber: 12, href: "/" },
-  { label: "Đại hội võ lâm văn phòng", date: "", time: "16/04/2024", curentNumber: 12, maxNumber: 12, href: "/" },
-  { label: "Giao lưu FPT", date: "", time: "16/04/2024", curentNumber: 0, maxNumber: 12, href: "/" },
-  { label: "Đấm đá túi bụi", date: "", time: "16/04/2024", curentNumber: 0, maxNumber: 12, href: "/" }]
+  { label: "Kèo anh em văn phòng đấm nhau", date: "2024-04-23T16:53:35.358Z", curentNumber: 1, maxNumber: 12, href: "/" },
+  { label: "Đại hội võ lâm văn phòng", date: "2024-04-23T16:53:35.358Z", curentNumber: 12, maxNumber: 12, href: "/" },
+  { label: "Giao lưu FPT", date: "2024-04-23T16:53:35.358Z", curentNumber: 6, maxNumber: 12, href: "/" },
+  { label: "Đấm đá túi bụi", date: "2024-04-23T16:53:35.358Z", curentNumber: 0, maxNumber: 12, href: "/" }]
 
-const items: MenuProps['items'] = listItems.map((item, index) => {
+const items = listItems.map((item, index) => {
   return {
     key: `${index}`,
     label: `${item.label}` ?? '',
+    date: `${item.date}` ?? '',
     curentNumber: item.curentNumber ?? 0,
     maxNumber: item.maxNumber ?? 12,
     href: `${item.href}`,
-    // status: item.status,
   }
 });
-
-function calculateMember(curentNumber: string, maxNumber: string): boolean {
-  return curentNumber === maxNumber;
-}
 
 function YourMatch() {
   return (
@@ -49,16 +41,16 @@ function YourMatch() {
         {
           items?.map((item, index) => {
             if (!item) return null;
-            const isFull = calculateMember(item.curentNumber, item.maxNumber);
+            const isFull = item.curentNumber === item.maxNumber;
             return <li key={item?.key}>
               <S.ItemsContainer>
                 <S.CustomCalendarOutlined $status={isFull} />
                 <S.MiddleInfor>
                   <S.CustomText $status={isFull}>{item.label}</S.CustomText>
-                  <S.CustomTimeText $status={isFull}>17:30 | 16/04/2024</S.CustomTimeText>
+                  <S.CustomTimeText $status={isFull}>{moment(item.date).format('LT')} | {moment(item.date).format('L')}</S.CustomTimeText>
                 </S.MiddleInfor>
                 <S.CustomTag $status={isFull} >
-                  <S.CustomTextTag >12/12</S.CustomTextTag>
+                  <S.CustomTextTag >{item.curentNumber}/{item.maxNumber}</S.CustomTextTag>
                 </S.CustomTag>
               </S.ItemsContainer>
             </li>
