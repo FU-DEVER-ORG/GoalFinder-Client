f71cfa214a8dca7e666e3f6954ccd695982c7ba1
Khangnvd <khangnguyen.030204@gmail.com>
2024-04-23 23:09:09 +0700

    feat/UI-Home-Feed

--- src/components/modules/Home/Feed/index.tsx
+++ src/components/modules/Home/Feed/index.tsx
@@ -3,9 +3,71 @@
 import BoxFlex from '@/components/core/common/BoxFlex';
 
 import * as S from './styles';
+import { Avatar, Button, Flex } from 'antd';
+import { CrownFilled, EllipsisOutlined, UserOutlined } from '@ant-design/icons';
 
 function Feed() {
-  return <BoxFlex>Feed</BoxFlex>;
+  return (
+    <S.FeedContainer>
+      <S.UserInfor>
+        <Avatar src="/images/layout/avatar.jpeg" size={40} />
+        <S.CustomFlex vertical>
+          <S.Name>Trần Văn Bảo Thắng</S.Name>
+          <S.FlexPrestige>
+            <S.CustomCrownFilled />
+            <S.PrestigeScore>120 Uy tín</S.PrestigeScore>
+          </S.FlexPrestige>
+        </S.CustomFlex>
+        <EllipsisOutlined></EllipsisOutlined>
+      </S.UserInfor>
+      <S.FlexMatchInfor>
+        <S.TitleMatchInfor>Kèo anh em văn phòng đấm nhau</S.TitleMatchInfor>
+        <S.ListMatchInfor>
+          <S.ItemMatchInfor>
+            <S.FlexBoxTextItem>
+              <S.BoldText>Thời gian:</S.BoldText>
+              <S.NoneBoldText>20/04/2024, 15:00</S.NoneBoldText>
+            </S.FlexBoxTextItem>
+          </S.ItemMatchInfor>
+          <S.ItemMatchInfor>
+            <S.FlexBoxTextItem>
+              <S.BoldText>Địa điểm:</S.BoldText>
+              <S.NoneBoldText>Sân ABC, Đường XYZ</S.NoneBoldText>
+            </S.FlexBoxTextItem>
+          </S.ItemMatchInfor>
+          <S.ItemMatchInfor>
+            <S.FlexBoxTextItem>
+              <S.BoldText>Cạnh tranh:</S.BoldText>
+              <S.NoneBoldText>Vui vẻ</S.NoneBoldText>
+            </S.FlexBoxTextItem>
+          </S.ItemMatchInfor>
+          <S.ItemMatchInfor>
+            <S.FlexBoxTextItem>
+              <S.BoldText>Khu vực:</S.BoldText>
+              <S.NoneBoldText>Hoà Hải, Ngũ Hành Sơn, Đà Nẵng</S.NoneBoldText>
+            </S.FlexBoxTextItem>
+          </S.ItemMatchInfor>
+          <S.ItemMatchInfor>
+            <S.FlexBoxTextItem>
+              <S.BoldText>Số lượng:</S.BoldText>
+              <S.NoneBoldText>12 người</S.NoneBoldText>
+            </S.FlexBoxTextItem>
+          </S.ItemMatchInfor>
+          <S.ItemMatchInfor>
+            <S.FlexBoxTextItem>
+              <S.BoldText>Tiền sân:</S.BoldText>
+              <S.NoneBoldText>500.000 VNĐ</S.NoneBoldText>
+            </S.FlexBoxTextItem>
+          </S.ItemMatchInfor>
+        </S.ListMatchInfor>
+      </S.FlexMatchInfor>
+      <Flex>
+        <p>Mô tả thêm:</p>
+        <p>Trận đấu được tổ chức nhằm tạo ra một môi trường thân thiện và hòa mình trong niềm đam mê bóng đá. Chúng ta sẽ cùng nhau tận hưởng niềm vui của trái bóng tròn, xây dựng mối quan hệ gắn kết và tạo ra những kỷ niệm đáng nhớ trên sân cỏ. Hãy đến và tham gia cùng chúng tôi! ⚽️</p>
+      </Flex>
+      <Button type="default">Huỷ đăng ký</Button>
+    </S.FeedContainer>
+  );
 }
 
 export default Feed;
--- src/components/modules/Home/Feed/styles.ts
+++ src/components/modules/Home/Feed/styles.ts
@@ -1,5 +1,103 @@
 import styled from 'styled-components';
+import BoxFlex from '@/components/core/common/BoxFlex';
+import { Flex } from 'antd';
+import { CrownFilled } from '@ant-design/icons';
 
-export const NewComponent = styled.div`
-  // Add your styles here
+export const FeedContainer = styled(BoxFlex)`
+  width: 712px;
+  border-radius: 10px;
+  padding: 20px;
+
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+`;
+
+export const UserInfor = styled(Flex)`
+  width: 100%;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  gap: 10px;
+`;
+
+export const CustomFlex = styled(Flex)`
+  width: 588px;
+  height: 35px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: end;
+  flex-grow: 1;
+`;
+
+export const Name = styled.p`
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 19.36px;
+`;
+
+export const FlexPrestige = styled(Flex)`
+  display: flex;
+  flex-direction: row;
+  align-items: end;
+  gap: 4px;
+`;
+
+export const CustomCrownFilled = styled(CrownFilled)`
+  color: #fcaa47 !important;
+`;
+
+export const PrestigeScore = styled.span`
+  font-size: 12px;
+  font-weight: 400;
+  line-height: 14.52px;
+  text-align: center;
+`;
+
+export const FlexMatchInfor = styled(Flex)`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+`;
+
+export const TitleMatchInfor = styled.p``;
+
+export const ListMatchInfor = styled.ul`
+  padding: 0;
+
+  column-count: 2;
+  list-style-position: inside;
+  ::marker {
+    padding-left: 0px 0px 0px 0px !important;
+  }
+`;
+
+export const ItemMatchInfor = styled.li`
+  margin-bottom: 12px;
+
+  line-height: 1.2;
+`;
+
+export const FlexBoxTextItem = styled.div`
+  width: 100%;
+  max-width: 336px;
+
+  display: inline-flex;
+  flex-direction: row;
+  gap: 8px;
+`;
+
+export const BoldText = styled.p`
+  font-weight: 500;
+`;
+
+export const NoneBoldText = styled.p`
+  font-weight: 400;
+  
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
 `;
