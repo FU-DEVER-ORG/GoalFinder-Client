7ac671e313d09c54f0575653e4563ef4906fc8af
HuyTran <tranquanghuyit101@gmail.com>
2024-04-24 13:39:55 +0700

    save code

--- src/components/core/layouts/MainLayout/NotificationDropdown/index.tsx
+++ src/components/core/layouts/MainLayout/NotificationDropdown/index.tsx
@@ -1,5 +1,5 @@
-import { Flex } from 'antd';
-
+import { Avatar, Flex, Divider } from 'antd';
+import React from 'react';
 import * as S from './styles';
 
 function NotificationDropdown() {
@@ -12,6 +12,77 @@ function NotificationDropdown() {
           <S.ButtonNoti>Chưa xem</S.ButtonNoti>
         </Flex>
       </S.Heading>
+      <Divider />
+      <S.Body>
+        <Flex gap={20}>
+        <S.AvatarContainer>
+          <Avatar size={60} 
+            src={'https://images-ext-1.discordapp.net/external/1cC__FAMCJpeDcSUazZGKkkO2YkJuSNn23fqWZmmN8U/https/res.cloudinary.com/dy1uuo6ql/image/upload/v1713897426/nk4zyhnkf8yghycffnqs.jpg?format=webp&width=662&height=662'} />
+        </S.AvatarContainer>
+            <div>
+              <h4>Huy Tran đã gửi đơn đăng ký vào Kèo anh em văn phòng đấm nhau</h4>
+              <h5>Hôm nay lúc 06:38</h5>
+            </div>
+            <S.Ellipse>
+             .
+            </S.Ellipse>
+        </Flex>
+
+        <Flex gap={20}>
+        <S.AvatarContainer>
+          <Avatar size={60} 
+            src={'https://images-ext-1.discordapp.net/external/1cC__FAMCJpeDcSUazZGKkkO2YkJuSNn23fqWZmmN8U/https/res.cloudinary.com/dy1uuo6ql/image/upload/v1713897426/nk4zyhnkf8yghycffnqs.jpg?format=webp&width=662&height=662'} />
+        </S.AvatarContainer>
+          <div>
+            <h4>Huy Tran đã gửi đơn đăng ký vào Kèo anh em văn phòng đấm nhau</h4>
+            <h5>Hôm nay lúc 06:38</h5>
+          </div>
+        </Flex>
+
+        <Flex gap={20}>
+        <S.AvatarContainer>
+          <Avatar size={60} 
+            src={'https://images-ext-1.discordapp.net/external/1cC__FAMCJpeDcSUazZGKkkO2YkJuSNn23fqWZmmN8U/https/res.cloudinary.com/dy1uuo6ql/image/upload/v1713897426/nk4zyhnkf8yghycffnqs.jpg?format=webp&width=662&height=662'} />
+        </S.AvatarContainer>
+          <div>
+            <h4>Huy Tran đã gửi đơn đăng ký vào Kèo anh em văn phòng đấm nhau</h4>
+            <h5>Hôm nay lúc 06:38</h5>
+          </div>
+        </Flex>
+
+        <Flex gap={20}>
+        <S.AvatarContainer>
+          <Avatar size={60} 
+            src={'https://images-ext-1.discordapp.net/external/1cC__FAMCJpeDcSUazZGKkkO2YkJuSNn23fqWZmmN8U/https/res.cloudinary.com/dy1uuo6ql/image/upload/v1713897426/nk4zyhnkf8yghycffnqs.jpg?format=webp&width=662&height=662'} />
+        </S.AvatarContainer>
+          <div>
+            <h4>Huy Tran đã gửi đơn đăng ký vào Kèo anh em văn phòng đấm nhau</h4>
+            <h5>Hôm nay lúc 06:38</h5>
+          </div>
+        </Flex>
+
+        <Flex gap={20}>
+        <S.AvatarContainer>
+          <Avatar size={60} 
+            src={'https://images-ext-1.discordapp.net/external/1cC__FAMCJpeDcSUazZGKkkO2YkJuSNn23fqWZmmN8U/https/res.cloudinary.com/dy1uuo6ql/image/upload/v1713897426/nk4zyhnkf8yghycffnqs.jpg?format=webp&width=662&height=662'} />
+        </S.AvatarContainer>
+          <div>
+            <h4>Huy Tran đã gửi đơn đăng ký vào Kèo anh em văn phòng đấm nhau</h4>
+            <h5>Hôm nay lúc 06:38</h5>
+          </div>
+        </Flex>
+
+        <Flex gap={20}>
+        <S.AvatarContainer>
+          <Avatar size={60} 
+            src={'https://images-ext-1.discordapp.net/external/1cC__FAMCJpeDcSUazZGKkkO2YkJuSNn23fqWZmmN8U/https/res.cloudinary.com/dy1uuo6ql/image/upload/v1713897426/nk4zyhnkf8yghycffnqs.jpg?format=webp&width=662&height=662'} />
+        </S.AvatarContainer>
+          <div>
+            <h4>Huy Tran đã gửi đơn đăng ký vào Kèo anh em văn phòng đấm nhau</h4>
+            <h5>Hôm nay lúc 06:38</h5>
+          </div>
+        </Flex>
+      </S.Body>
     </S.NotificationWrapper>
   );
 }
--- src/components/core/layouts/MainLayout/NotificationDropdown/styles.ts
+++ src/components/core/layouts/MainLayout/NotificationDropdown/styles.ts
@@ -23,15 +23,16 @@ export const Heading = styled.div`
 
   h4 {
     font-size: 24px;
-    font-weight: 500;
+    font-weight: 600;
   }
+
 `;
 
 export const ButtonNoti = styled.button<{ $active?: boolean }>`
   padding: 4px 16px;
 
   font-size: 16px;
-  font-weight: 500;
+  font-weight: 600;
 
   border: none;
   border-radius: 20px;
@@ -39,4 +40,46 @@ export const ButtonNoti = styled.button<{ $active?: boolean }>`
   background-color: ${({ $active }) => ($active ? '#A7FFB1' : '#fff')};
 
   cursor: pointer;
+`;
+
+export const Body = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  h3 {
+    position: absolute;
+    center:;
+    right: 0px;
+    width: 10px;
+    height: 10px;
+    color: #0031E0;
+  }
+
+  h4 {
+    font-size: 16px;
+    font-weight: 500;
+  }
+
+  h5 {
+    padding-bottom: 12px;
+    font-size: 12px;
+    font-weight: 400;
+    color: #0031E0;
+  }
+`;
+
+export const AvatarContainer = styled.div`
+  display: flex;
+  width: 60px;
+  height: 60px;
+  border-radius: 50%;
+  }
+`;
+
+export const Ellipse = styled.ellipse<{ $active?: boolean }>`
+
+  display: flex;
+  font-size: 100px;
+
+  color: ${({ $active }) => ($active ? '#fff' : '#0031E0')};
 `;
